44560be3274aa351402bfa23c176cbe8122fb426
ssamyne <crowszero22@gmail.com>
2022-09-11 18:28:08 +0700

    feat: footer complete

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,11 @@
-import './Navbar.scss';
+import './Footer.scss';
 
 const Footer = () => {
-  return <div className='footer'></div>;
+  return (
+    <div className='footer'>
+      <p className='footer__copyright'>&copy; Copyright 2022. Klitsana C.</p>
+    </div>
+  );
 };
 
 export default Footer;
