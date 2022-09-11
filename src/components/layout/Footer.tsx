c7fc3ad2aa5845782f910d75ea087661ac8a8365
ssamyne <crowszero22@gmail.com>
2022-09-11 19:29:04 +0700

    feat: edit footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,9 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <div className='footer'>
-      <p className='footer__copyright'>&copy; Copyright 2022. Klitsana C.</p>
+      <p className='footer__copyright'>
+        &copy; Copyright 2022 Production House. Create by Klitsana C.
+      </p>
     </div>
   );
 };
